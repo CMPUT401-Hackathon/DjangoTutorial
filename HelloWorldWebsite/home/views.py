6e5167115b57d5bba5b200272787cf9fccfaf619
Cecilia Wei <ceciliawei@Cecilias-MacBook-Pro.local>
2019-09-08 12:08:50 -0600

    hope it works

--- HelloWorldWebsite/home/views.py
+++ HelloWorldWebsite/home/views.py
@@ -132,7 +132,7 @@ class Home(generic.DetailView):
             related.append(instance)
 
         return render(request, "home/artist.html", {
-            'Artists': Q,
+            'Artist': Q,
             'Related':related
 
         })
